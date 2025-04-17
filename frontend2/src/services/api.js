fd004578e62e9ac4bc2ae37ffc1b7d1cb8123492
Thokozani84 <atmospherequest84@gmail.com>
2025-04-17 12:04:07 +0200

    point API to production backend2

--- frontend2/src/services/api.js
+++ frontend2/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust if your backend is hosted elsewhere
+  baseURL: 'https://backend200.onrender.com/api', // ✅ Now pointing to your Render backend
 });
 
 export const getVideosByCategory = async (category) => {
