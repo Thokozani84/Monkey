ed94f2221440603af18202fec0e0a3bd3700bde0
Thokozani84 <atmospherequest84@gmail.com>
2025-04-17 10:55:34 +0200

    Updated frontend and backend with latest changes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const videoRoutes = require('./routes/videoRoutes');
-
+const port = process.env.PORT || 5000
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -16,5 +16,5 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true
 }).then(() => {
     console.log('MongoDB connected');
-    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+    app.listen(port, () => console.log('Server running on http://localhost:5000'));
 }).catch(err => console.error(err));
